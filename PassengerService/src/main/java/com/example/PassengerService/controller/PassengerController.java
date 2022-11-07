7ca7dd5e7f94288eb2da78d663766aeb52432a90
BaoDoan(Yone) <baodakmil123@gmail.com>
2022-11-07 13:24:21 +0700

    finish retry to call passenger service

--- PassengerService/src/main/java/com/example/PassengerService/controller/PassengerController.java
+++ PassengerService/src/main/java/com/example/PassengerService/controller/PassengerController.java
@@ -4,7 +4,6 @@ import com.example.PassengerService.VO.ResponseTemplateVO;
 import com.example.PassengerService.entity.Passenger;
 import com.example.PassengerService.service.PassengerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
--- PassengerService/src/main/java/com/example/PassengerService/service/PassengerService.java
+++ PassengerService/src/main/java/com/example/PassengerService/service/PassengerService.java
@@ -5,6 +5,8 @@ import com.example.PassengerService.entity.Billing;
 import com.example.PassengerService.entity.Passenger;
 import com.example.PassengerService.repository.PassengerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -20,16 +22,16 @@ public class PassengerService {
     public Passenger addPassenger(Passenger passenger) {
         return passengerRepository.save(passenger);
     }
-
+    int count = 0;
+    @Retryable(value = Exception.class, maxAttempts = 10, backoff = @Backoff(value = 1000))
     public ResponseTemplateVO getPassengerWithBilling(Integer passengerId)
     {
-        System.out.println("start api billing");
+        System.out.println("recall" + ++count);
+
         ResponseTemplateVO vo = new ResponseTemplateVO();
         Passenger passenger = passengerRepository.findPassengerById(passengerId);
 
         Billing billing = restTemplate.getForObject("http://localhost:8080/billing/passenger/" + passengerId, Billing.class);
-
-        System.out.println("meo" + billing);
 
         vo.setPassenger(passenger);
         vo.setBilling(billing);
